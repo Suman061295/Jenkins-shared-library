0805aa7ddda947ca353ca66ee911add9fdaf20ad
Suman061295 <gsuman1995@gmail.com>
2025-08-17 16:55:44 +0530

    updated

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,10 +1,12 @@
 def call(Map config) {
     echo "URL: ${config.url}"
     echo "Branch: ${config.branch}"
+    if ( ${config.branch} == "main" ) {}
     checkout([
         $class: 'GitSCM',
         branches: [[name: config.branch ]],
         userRemoteConfigs: [[ url: config.url,
-        credentialsId: config.credentialsId ]],
-    ])
+        credentialsId: config.credentialsId ]]])
+} else {
+    echo "Branch ${config.branch} is not allowed."
 }
